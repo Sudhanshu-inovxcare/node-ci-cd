00b1811c87cd0d3d99e116279262bb1a770af22b
Sudhanshu <Sudhanshu@innovxcare.ai>
2026-07-29 18:11:06 +0530

    changes test file

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,13 @@
-const request = require("supertest")
-const app = require("../src/app")
-const { describe, test } = require("node:test")
+const request = require("supertest");
+const app = require("../src/app");
 
-describe("Health-Endpoint", () => {
-    test('GET /health should return UP', async () => {
-        const response = await request(app).get("/health");
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({
-            status:"UP"
-        })
-    })
-})
+describe("Health Endpoint", () => {
+  test("GET /health should return UP", async () => {
+    const response = await request(app).get("/health");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      status: "UP",
+    });
+  });
+});
